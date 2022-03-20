7ab20ddc0319d1b67addfcdd42fdb0fc851f4586
Kalir888 <kalir888@gmail.com>
2022-03-20 16:48:54 +0530

    Calc Daily Emp Wage

--- Day9Assignment/src/com/company/Main.java
+++ Day9Assignment/src/com/company/Main.java
@@ -1,19 +1,26 @@
 package com.company;
 
 import java.util.Random;
+import java.util.zip.DataFormatException;
 
 public class Main {
+
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOUR = 8;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         int IS_PRESENT = 1;
-        Random random = new Random();
-        int attendance = random.nextInt()%2;
 
-        if (attendance == IS_PRESENT)
-            System.out.println("Employee is present");
-        else
+        Random random = new Random();
+        int attendance = Math.abs(random.nextInt()%2);
+        int dailyEmpWage;
+
+        if(attendance == IS_PRESENT) {
+            dailyEmpWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Daily Employee Wage is = " + dailyEmpWage);
+        } else
             System.out.println("Employee is absent");
     }
 }
