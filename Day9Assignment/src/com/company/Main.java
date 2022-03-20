6867ab2ef470189659eace2fac10d39460017b15
Kalir888 <kalir888@gmail.com>
2022-03-20 20:05:12 +0530

    Employee Wage for multiple Companies

--- Day9Assignment/src/com/company/Main.java
+++ Day9Assignment/src/com/company/Main.java
@@ -1,18 +1,17 @@
 package com.company;
 
-
-import java.util.Random;
-
-
 public class Main {
 
-
     public static void main(String[] args) {
+        
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        EmployeeWage employeeWage = new EmployeeWage();
-        int employee1Wage = employeeWage.calculate();
+        EmployeeWage cms = new EmployeeWage("CMS",40,8,4,26,150);
+        EmployeeWage sipl = new EmployeeWage("SIPL",50,8,4,26,150);
+        int employee1Wage = cms.calculate();
+        int employee2Wage = sipl.calculate();
 
-        System.out.println("Employee1 Wage per month = " + employee1Wage);
+        System.out.println("EmployeeWagePerMonth for company "+cms.companyName+" = " + employee1Wage);
+        System.out.println("EmployeeWagePerMonth for company "+sipl.companyName+" = " + employee2Wage);
     }
 }
