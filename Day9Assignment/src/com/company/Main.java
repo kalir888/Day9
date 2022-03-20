3b4f83b99acd53a26fddaa0a8db3996607ff55e5
Kalir888 <kalir888@gmail.com>
2022-03-20 16:25:11 +0530

    employee attendance check finished

--- Day9Assignment/src/com/company/Main.java
+++ Day9Assignment/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        int IS_PRESENT = 1;
+        Random random = new Random();
+        int attendance = random.nextInt()%2;
+
+        if (attendance == IS_PRESENT)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
