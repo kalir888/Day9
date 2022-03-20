773319562de9b9083986f96dcb86c27b4331eab0
Kalir888 <kalir888@gmail.com>
2022-03-20 20:10:48 +0530

    package name changed

--- Day9Assignment/src/com/employeewage/EmployeeWage.java
+++ Day9Assignment/src/com/employeewage/EmployeeWage.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.employeewage;
 
 import java.util.Random;
 
--- Day9Assignment/src/com/employeewage/Main.java
+++ Day9Assignment/src/com/employeewage/Main.java
@@ -1,9 +1,9 @@
-package com.company;
+package com.employeewage;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Welcome to Employee Wage Computation Program");
 
         EmployeeWage cms = new EmployeeWage("CMS",40,8,4,26,150);
